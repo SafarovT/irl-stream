127f64b275133cf6201afffe6cd0f2447cd04a54
Tahir Safarov <79829434+SafarovT@users.noreply.github.com>
2024-05-13 13:43:09 +0300

    Добавил stateManager с тестовыми данными

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    connectLogger,
+    createCtx,
+} from '@reatom/framework'
+import {reatomContext} from '@reatom/npm-react'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {MapPage} from "./pages/mapPage/MapPage";
+
+const ctx = createCtx()
+connectLogger(ctx)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
 root.render(
-  <React.StrictMode>
-    <MapPage />
-  </React.StrictMode>
+    <React.StrictMode>
+        <reatomContext.Provider value={ctx}>
+            <MapPage />
+        </reatomContext.Provider>
+    </React.StrictMode>
 )
 
 // to log performance: reportWebVitals(console.log)
---
+++ src/model/model.ts
@@ -0,0 +1,31 @@
+import {
+	action,
+	atom,
+} from '@reatom/framework'
+
+const initState = localStorage.getItem('irl-stream') ?? ''
+
+const testDataAtom = atom(initState, 'testDataAtom')
+
+const testDataSubAtom = atom((ctx) => {
+	const data = ctx.spy(testDataAtom)
+	return data ? `Test data = ${data}` : ''
+}, 'testDataSubAtom')
+
+const setTestData = action((ctx, data: string) => {
+	testDataAtom(ctx, data)
+}, 'setTestData')
+
+const modelAtoms = {
+	testDataAtom,
+	testDataSubAtom,
+}
+
+const modelActions = {
+	setTestData,
+}
+
+export {
+	modelActions,
+	modelAtoms,
+}
